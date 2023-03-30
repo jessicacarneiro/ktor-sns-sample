151f3f7090c66b38954ab656be6e9f20f1f542a0
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2023-03-30 17:50:06 -0300

    Fix logging and version issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.20"
+    kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
+
+val logbackVersion: String by project
 
 group = "com.example"
 version = "0.0.1"
@@ -22,6 +24,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-logging-jvm:2.2.1")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
@@ -29,4 +33,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("aws.sdk.kotlin:sns-jvm:0.21.3-beta")
 }
